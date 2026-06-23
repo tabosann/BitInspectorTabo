4e34b1a46e49da3be7264969e8e968114a744207
tabosann <tabosann@example.com>
2026-06-23 16:38:55 +0900

    [EDT]WinUI_Template_2026の導入と初期構想のセットアップ

--- BitInspectorTabo/Pages/ErrorPage.xaml.cs
+++ BitInspectorTabo/Pages/ErrorPage.xaml.cs
@@ -1,4 +1,3 @@
-using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -13,24 +12,20 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using BitInspectorTabo.Pages;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace BitInspectorTabo
+namespace BitInspectorTabo.Pages
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class ErrorPage : Page
     {
-        public MainWindow()
+        public ErrorPage()
         {
             InitializeComponent();
-            ExtendsContentIntoTitleBar = true;
-            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
-            FrameMain.Navigate(typeof(MainPage));
         }
     }
 }
---
+++ BitInspectorTabo/Pages/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Effects;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BitInspectorTabo.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel MPVM { get; } = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GridRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            FrameContent.Navigate(typeof(ErrorPage));
+            MPVM.FrameContent = FrameContent;
+        }
+
+        private void TitleBar_PaneToggleRequested(TitleBar sender, object args)
+        {
+            NavigationViewMain.PaneDisplayMode = NavigationViewMain.PaneDisplayMode switch
+            {
+                NavigationViewPaneDisplayMode.LeftCompact => NavigationViewPaneDisplayMode.Left,
+                _ => NavigationViewPaneDisplayMode.LeftCompact
+            };
+        }
+    }
+
+    public partial class MainPageViewModel : ObservableObject
+    {
+        public Frame? FrameContent { get; set; } = null;
+
+        [ObservableProperty]
+        public partial object NavigationViewMainSelectedItem { get; set; }
+        partial void OnNavigationViewMainSelectedItemChanged(object value)
+        {
+            var item = value as NavigationViewItem;
+            if(item == null || FrameContent == null)
+            {
+                return;
+            }
+
+            switch(item.Tag)
+            {
+                case "Inspector":
+                    FrameContent.Navigate(typeof(InspectorPage));
+                    break;
+                case "Settings":
+                    FrameContent.Navigate(typeof(SettingsPage));
+                    break;
+                default:
+                    FrameContent.Navigate(typeof(ErrorPage));
+                    break;
+            }
+        }
+    }
+}
